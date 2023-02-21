6704d0ef6f5c11ba300ac8b90cbfe28de5440de7
gavin <kwongavin@icloud.com>
2023-02-20 20:14:59 -0800

    Initial Commit

---
+++ Github-Test/Github_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_TestApp.swift
+//  Github-Test
+//
+//  Created by Gavin Kwon on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
